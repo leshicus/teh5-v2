a2e93fd27e58743c49d5b9cce5d6288585ef3bce
leshicus <leshicus@gmail.com>
2018-11-17 00:30:03 +0300

    PG routing

---
+++ src/playground/First.js
@@ -0,0 +1,12 @@
+//@flow
+import * as React from 'react'
+
+const styles = {
+  root: {},
+}
+
+export class First extends React.Component<{}> {
+  render() {
+    return <div style={styles.root}>1</div>
+  }
+}
