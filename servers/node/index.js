c705fbfa4eef123ee0b8002f7719780994d490c8
leshicus <leshicus@gmail.com>
2018-12-02 19:27:33 +0300

    add debug settings to vscode

--- servers/node/index.js
+++ servers/node/index.js
@@ -1,11 +1,11 @@
-const Koa = require('koa');
+const Koa = require('koa')
 
-const app = new Koa();
+const app = new Koa()
 
 app.use(async ctx => {
-  ctx.body = 'Hello from website0';
-  console.log('ctx', ctx);
-});
+  ctx.body = 'Hello from website0'
+  console.log('ctx', ctx)
+})
 
-app.listen(3000);
-console.log('listen: 3000');
+app.listen(3000)
+console.log('listen: 3000')
